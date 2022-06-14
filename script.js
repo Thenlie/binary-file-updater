533901b18cf0900caab96ac8e9023b4ecb287816
Leithen <leithen113@gmail.com>
2022-06-13 18:50:49 -0600

    Cleaned up functions, consolidating code

--- script.js
+++ script.js
@@ -2,15 +2,36 @@ const fs = require('fs');
 const flaps = require('./FLAPS.json');
 const egr = require('./EGR.json');
 
-// read bin file
-const decryptor  = fs.readFileSync('./03G906016B_orig.bin');
-const binArr = toArrayBuffer(decryptor);
-
-// convert bin buffer to array
-const normalArr = Array.from(binArr);
-
-let matchArr = [];
 let answer = [];
+
+// read bin file and return arr of decimal values
+function readFile(fileName) {
+    // read bin file
+    const decryptor  = fs.readFileSync(fileName);
+    const binArr = toArrayBuffer(decryptor);
+    
+    // convert bin buffer to array
+    const normalArr = Array.from(binArr);
+    return normalArr;
+};
+
+// write new hex to bin file
+function writeFile(data) {
+    fs.writeFileSync('./03G906016B_orig.bin', data, {
+        encoding: 'hex',
+        flag: 'w'
+    }, function(err) {
+        if (err) {
+            console.error(err);
+        };
+    });
+    console.log("File written successfully\n");
+};
+
+let file = readFile('./03G906016B_orig.bin');
+let match1 = matchData(file, flaps, 'flaps');
+let match2 = matchData(file, egr, 'egr');
+console.log(match1, match2);
 
 // convert buffer to ArrayBuffer
 function toArrayBuffer(buf) {
@@ -60,23 +81,26 @@ function toUnitArray() {
     return newArr;
 }
 
-// match strings in flaps JSON
-function getFlaps() {
-    for (let i = 0; i < flaps.strings.length; i++) {
-        let decArr = toDecimalArray(flaps.strings[i]);
+// match strings in file to JSON data
+function matchData(file, data, type) {
+    matchArr = [];
+    for (let i = 0; i < data.strings.length; i++) {
+        let decArr = toDecimalArray(data.strings[i]);
         let start = 0;
         let end = 0;
         let c = 0;
-        for (let j = 0; j < binArr.length; j++) {
-            if (decArr[c] === binArr[j]) {
+        for (let j = 0; j < file.length; j++) {
+            if (decArr[c] === file[j]) {
                 // matches all bytes in a row
                 if (c === decArr.length - 1) {
                     end = j;
                     let obj = {
+                        // position of matched array in file
                         start: start,
                         end: end,
+                        // position in "strings" array
                         index: i,
-                        type: 'flaps'
+                        type: type
                     }
                     matchArr.push(obj);
                     break;
@@ -95,45 +119,7 @@ function getFlaps() {
             };
         };
     };
-};
-
-// match strings in egr JSON
-function getEgrs() {
-    for (let i = 0; i < egr.strings.length; i++) {
-        let decArr = toDecimalArray(egr.strings[i]);
-        let start = 0;
-        let end = 0;
-        let c = 0;
-        for (let j = 0; j < binArr.length; j++) {
-            if (decArr[c] === binArr[j]) {
-                // matches all bytes in a row
-                if (c === decArr.length - 1) {
-                    // set end value to j index
-                    end = j;
-                    let obj = {
-                        start: start,
-                        end: end,
-                        index: i,
-                        type: 'egr'
-                    }
-                    matchArr.push(obj);
-                    break;
-                // matches a byte
-                } else {
-                    if (c === 0) {
-                        // set start value to j index
-                        start = j;
-                    };
-                    c++;
-                }
-            } else {
-                if (c !== 0) {
-                    c = 0;
-                    j = j - c;
-                };
-            };
-        };
-    };
+    return matchArr;
 };
 
 // update array based on RPM values
@@ -158,22 +144,9 @@ function updateArr(arr) {
     };
 };
 
-// write new hex to bin file
-function writeFile(data) {
-    fs.writeFileSync('./03G906016B_orig.bin', data, {
-        encoding: 'hex',
-        flag: 'w'
-    }, function(err) {
-        if (err) {
-            console.error(err);
-        };
-    });
-    console.log("File written successfully\n");
-};
-
 function run() {
-    getFlaps();
-    getEgrs();
+    matchFlaps();
+    matchEgrs();
     console.log(matchArr);
     let hexArr = toHex(normalArr);
     updateArr(hexArr);
@@ -183,4 +156,4 @@ function run() {
     // writeFile(newArr);
 };
 
-run();
+// run();
